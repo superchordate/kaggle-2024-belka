5a9ba1428e33382a6d5ede015b89e6414fe4fef9
Bryce Chamberlain <superchordate@gmail.com>
2024-06-24 22:24:02 -0600

    Add print_results to net.

--- scripts/2a-fit-net.py
+++ scripts/2a-fit-net.py
@@ -1,7 +1,7 @@
 # https://www.kaggle.com/code/jetakow/home-credit-2024-starter-notebook/notebook
 
 from modules.datasets import get_loader
-from modules.net import train, run_val
+from modules.net import train, run_val, print_results
 from modules.score import kaggle_score
 from modules.utils import pad0, dircreate
 import os, torch
@@ -19,7 +19,7 @@ options = {
 #TODO - compare to 1 epcoch 15 files (same # batches/duration)
 
 # train models. 
-nets = {}
+nets = {}   
 for protein_name in ['sEH', 'BRD4', 'HSA']:
     
     model_path = f'out/net/net-{run_name}-{protein_name}.pt'
@@ -36,11 +36,21 @@ for protein_name in ['sEH', 'BRD4', 'HSA']:
     else:
         nets[protein_name] = torch.jit.load(model_path).eval()
 
+# get metrics for train so we can see if we are over-fitting.
+if 'n_files' in options:
+    print('measure train accuracy for sEH')
+    ids, labels, scores = run_val(
+        get_loader('out/train/val/', 'sEH', options = options), 
+        nets[protein_name]
+    )
+    print_results(labels, scores)
+
 # check val to get expected score.
 print('val')
 solution = []
 submission = []
-for protein_name in ['sEH', 'BRD4', 'HSA']:
+for protein_name in ['sEH']:
+#for protein_name in ['sEH', 'BRD4', 'HSA']:
     
     ids, labels, scores = run_val(
         get_loader('out/train/val/', protein_name, options = options), 
@@ -64,6 +74,7 @@ solution = pd.concat(solution)
 submission = pd.concat(submission)
 expected_score = kaggle_score(solution, submission, "id")
 print(f'expected score: {expected_score :.2f}')
+print_results(solution['binds'], submission['binds'])
 
 # run test to get the actual submission. 
 print('submit')
@@ -84,6 +95,5 @@ for protein_name in ['sEH', 'BRD4', 'HSA']:
 
 dircreate('out/submit')
 submission = pd.concat(submission).sort_values('id')
-# submission.to_parquet(f'out/test/submission-{datetime.today().strftime("%Y%m%d")}-{pad0(int(expected_score*100))}.parquet', index = False)
 submission.to_parquet(f'out/submit/submission-{datetime.today().strftime("%Y%m%d")}-{run_name}-{pad0(int(expected_score*100))}.parquet', index = False)
 
