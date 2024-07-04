3df0f2218d95c649fb6b0de12ca5e9e38d5fe87c
Bryce Chamberlain <superchordate@gmail.com>
2024-07-04 17:41:27 -0600

    Shrink full train mols.

--- scripts/2b-shrink-mols.py
+++ scripts/2b-shrink-mols.py
@@ -3,16 +3,23 @@
 
 import polars as pl
 
-train_train_mols = pl.read_parquet('out/train/train/mols.parquet')
+def reducemols(path):
 
-train_train_mols_binds = train_train_mols.filter(
-    pl.col(f'binds_sEH') | pl.col(f'binds_BRD4') | pl.col(f'binds_HSA')
-)
-train_train_mols = train_train_mols.filter(
-    (pl.col(f'binds_sEH') | pl.col(f'binds_BRD4') | pl.col(f'binds_HSA')).not_()
-)
-train_train_mols_binds.shape[0] # 1.3M binds, I would like a ratio of 25% binds so let's sample 3x this amount.
+    mols = pl.read_parquet(path)
+    
+    mols_binds = mols.filter(
+        pl.col(f'binds_sEH') | pl.col(f'binds_BRD4') | pl.col(f'binds_HSA')
+    )
+    
+    mols = mols.filter(
+        (pl.col(f'binds_sEH') | pl.col(f'binds_BRD4') | pl.col(f'binds_HSA')).not_()
+    )
+    #mols_binds.shape[0] # 1.3M binds, I would like a ratio of 25% binds so let's sample 3x this amount.
+    
+    reduced_mols = pl.concat([mols_binds, mols.sample(mols_binds.shape[0] * 3)])
+    
+    reduced_mols.write_parquet(path)
+    
 
-reduced_mols = pl.concat([train_train_mols_binds, train_train_mols.sample(train_train_mols_binds.shape[0] * 3)])
-
-reduced_mols.write_parquet('out/train/train/mols.parquet')
+#reducemols('out/train/train/mols.parquet')
+reducemols('out/train/mols.parquet')
